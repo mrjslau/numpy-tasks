1bbc41c7d069d1250b65ce27c94482b5ebd5fef0
marijusiMac <marijuslau@yahoo.com>
2018-03-06 20:38:01 +0200

    All numpy tasks complete

--- numpytasks.py
+++ numpytasks.py
@@ -10,22 +10,22 @@ def test_run():
     new_line()
 
 # 2. Sugeneruokite masyvą dydžio 3n ir užpildykite jį cikliniu šablonu [1, 2, 3].
-    b = np.tile([1, 2, 3], 5)
+    n = 6
+    b = np.tile([1, 2, 3], n)
     print("2. CYCLE: \n", b)
     new_line()
 
 # 3. Sukurkite masyvą iš pirmųjų 10 nelyginių sveikųjų skaičių.
-    c = np.arange(10)[1::2]
+    c = np.arange(20)[1::2]
     print("3. ODD: \n", c)
     new_line()
 
 # 4. Sukurkite masyvą dydžio 10 x 10 iš nulių ir "įrėminkite" jį vienetais.
     d = np.zeros((10, 10), dtype=int)
-    d[0].fill(1)
-    d[9].fill(1)
-    print("4. 10x10 ZEROS: \n", d)
+    d[::9].fill(1)
+    d[::,::9].fill(1)
+    print("4. 10x10 FRAME: \n", d)
     new_line()
-#-
 
 # 5. Sukurkite masyvą dydžio 8 x 8, kur 1 ir 0 išdėlioti šachmatine tvarka (panaudokite slicing+striding metodą).
     e = np.zeros((8, 8), dtype=int)
@@ -36,38 +36,42 @@ def test_run():
     # [start:end:step]
 
 # 6. Sukurkite masyvą dydžio n×n , kurio (i,j)-oji pozicija lygi i+j
-
-#-
+    n = 6
+    f = np.indices((n,n))
+    f_indexed = np.add(f[0], f[1])
+    print("6. i+j = POSITIONS: \n", f_indexed)
+    new_line()
 
 # 7. Sukurkite atsitiktinį masyvą dydžio 3×5 naudodami np.random.rand(3, 5) funkciją ir suskaičiuokite: sumą, eilučių sumą, stulpelių sumą.
-    f = np.random.rand(3, 5)
-    print("7. 3x5 RANDOM.RAND: \n", f)
-    fsum = f.sum()
-    print("Sum of all array elements: \n", fsum)
-    frowsum = np.sum(f, axis=1).tolist()
-    print("Sums of array rows: \n", frowsum)
-    fcolsum = np.sum(f, axis=0).tolist()
-    print("Sums of array columns: \n", fcolsum)
+    g = np.random.rand(3, 5)
+    print("7. 3x5 RANDOM.RAND: \n", g)
+    gsum = g.sum()
+    print("Sum of all array elements: \n", gsum)
+    growsum = np.sum(g, axis=1).tolist()
+    print("Sums of array rows: \n", growsum)
+    gcolsum = np.sum(g, axis=0).tolist()
+    print("Sums of array columns: \n", gcolsum)
     new_line()
 # 8. Sukurkite atsitiktinį masyvą dydžio 5×5 naudodami np.random.rand(5, 5). Surūšiuokite eilutes pagal antrąjį stulpelį. Tam pamėginkite apjungti masyvo slicing + argsort + indexing metodus.
-    g = np.random.rand(5, 5)
-    print("8. 5x5 RANDOM.RAND: \n", g)
-#-
+    h = np.random.rand(5, 5)
+    print("8. 5x5 RANDOM.RAND: \n", h)
+    h_sort = h[h[:,1].argsort()]
+    print("SORTED: \n", h_sort)
     new_line()
 
 # 9. Atvirkštinę matricą
-    h = np.array([[1,4],[-3,9]])
-    print("9. NORMAL: \n", h)
-    h_inv = np.linalg.inv(h)
-    print("INVERSION: \n", h_inv)
+    i = np.array([[1,4],[-3,9]])
+    print("9. NORMAL: \n", i)
+    i_inv = np.linalg.inv(i)
+    print("INVERSION: \n", i_inv)
     new_line()
 
 # 10.Apskaičiuokite matricos tikrines reikšmes ir tikrinį vektorių
-    i = np.array([[0,1],[-2,-3]])
-    print("10. MATRIX: \n", i)
-    i_w, i_v = np.linalg.eig(i)
-    print("EIGENVALUES: \n", i_w)
-    print("EIGENVECTORS: \n", i_v)
+    j = np.array([[0,1],[-2,-3]])
+    print("10. MATRIX: \n", j)
+    j_w, j_v = np.linalg.eig(j)
+    print("EIGENVALUES: \n", j_w)
+    print("EIGENVECTORS: \n", j_v)
     new_line()
 
 # 11.Pasirinktos funkcijos išvestinę
